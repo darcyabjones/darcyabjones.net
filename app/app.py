bbce951b09c98c506b851b91f988ec4fe5f665b8
Darcy Jones <darcy.ab.jones@gmail.com>
2015-03-28 23:02:53 +1100

    Edited project

--- app/app.py
+++ app/app.py
@@ -403,6 +403,22 @@ def page_not_found(e):
     return render_template("404.html", nav=nav("None")), 404
 
 
+@app.route('/termsofuse')
+def tou():
+    terms = {
+        "title": "Terms of use",
+        "terms": (
+            "All content provided on this blog is for informational purposes "
+            "only. The owner of this blog makes no representations as to the "
+            "accuracy or completeness of any information on this site or found"
+            " by following any link on this site. The owner will not be liable"
+            " for any errors or omissions in this information nor for the "
+            "availability of this information. The owner will not be liable "
+            "for any losses, injuries, or damages from the display or use of "
+            "this information. These terms and conditions of use are subject "
+            "to change at anytime and without notice.")
+        }
+
 #################################### Code ####################################
 
 if __name__ == '__main__':
